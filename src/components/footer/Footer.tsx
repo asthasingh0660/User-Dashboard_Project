dfbeb9301828975869c86c645954df56a68ea74e
asthasingh0660 <asthasingh0660@gmail.com>
2025-05-17 01:12:19 +0530

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Dashboard Project</span>
+      <span>© Astha Singh Dashboard</span>
+    </div>
+  )
+}
+
+export default Footer
